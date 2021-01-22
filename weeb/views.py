a3d1b163141e56fce97f8783a17a4f36a2216555
rootz491 <experimentyt1@gmail.com>
2021-01-23 00:43:36 +0530

    :checkbox: profile delete done

--- weeb/views.py
+++ weeb/views.py
@@ -100,14 +100,42 @@ def profile(request, username):
 def userDelete(request, username):
 
     if request.method == 'POST':
-        user = get_object_or_404(User, username=username)
-        form = userDeleteForm(request.POST or None, request.FILES or None)
+        try:
+            form = userDeleteForm(request.POST)
+            user = User.objects.get(username=username)
+            if request.user == user:
+                pass
+            else:
+                print('forbidden')
+                messages.add_message(request, messages.ERROR, 'forbidden')
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-        # check whether it's valid:
-        if form.is_valid():
-            thatUSer = form.save(commit=False)
-            passwd = thatUSer.get_password()
-            print(passwd)
+            # check whether it's valid:
+            if form.is_valid():
+                thatUSer = form.save(commit=False)
+                password = thatUSer.password
+                print(password)
+                if user.check_password(password):
+                    print('deleted!')
+                    # user.delete()
+                    messages.success(request, "The user is deleted")
+                    return reverse('login')
+                else:
+                    messages.add_message(request, messages.ERROR, 'Wrong password. please try again!')
+                    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        except User.DoesNotExist:
+            messages.add_message(request, messages.ERROR, 'User doesn\'t exist')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        except Exception as e:
+            messages.add_message(request, messages.ERROR, e)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+    # make a post request
+    return render(request, 'front.html')
+
 
 
 
